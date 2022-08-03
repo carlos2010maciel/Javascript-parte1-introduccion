8a33573d7090873a69856fd01245f22d4ddaf4ec
Horacio Carlos Maciel de Lima <carlos2010maciel@gmail.com>
2022-08-03 01:05:48 -0300

    Aplicados loop y estilos a los arrays

--- js/principal.js
+++ js/principal.js
@@ -1,36 +1,40 @@
+var pacientes = document.querySelectorAll(".paciente");
 
-var paciente = document.querySelector("#primer-paciente");
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-var tdIMC = paciente.querySelector(".info-imc");
+  var tdIMC = paciente.querySelector(".info-imc");
 
-pesoEsValido = true;
-alturaEsValida = true;
+  pesoEsValido = true;
+  alturaEsValida = true;
 
-//verdadero o falso --> verdadero
-if((peso < 0)||(peso > 1000)){
+  //verdadero o falso --> verdadero
+  if (peso < 0 || peso > 1000) {
     console.log("Peso incorrecto");
     tdIMC.textContent = "Peso incorrecto";
     pesoEsValido = false;
-}
+    paciente.classList.add("paciente-incorrecto");
+  }
 
-//verdadero o falso --> verdadero
-if((altura < 0)||(altura > 3.0)){
+  //verdadero o falso --> verdadero
+  if (altura < 0 || altura > 3.0) {
     console.log("Peso incorrecto");
     tdIMC.textContent = "Altura incorrecta";
     alturaEsValida = false;
-}
+    paciente.classList.add("paciente-incorrecto");
+  }
 
-//verdadero y verdadero --> verdadero
-//verdadero y falso --> falso
-//falso y falso --> falso
-if(pesoEsValido && alturaEsValida){
+  //verdadero y verdadero --> verdadero
+  //verdadero y falso --> falso
+  //falso y falso --> falso
+  if (pesoEsValido && alturaEsValida) {
     var imc = peso / (altura * altura);
-    tdIMC.textContent = imc;
+    tdIMC.textContent = imc.toFixed(2);
+  }
 }
-
